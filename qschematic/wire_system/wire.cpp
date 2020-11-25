7e6bb60d7a2a635ea439209144b646f0d6f127cc
Joel Bodenmann <joel.bodenmann@simulton.com>
2020-11-25 19:15:58 +0100

    Add Qt library version conditional for deprecated QLineF::intersect() function.
    Commit 3050ef09fd4ee623af526055e4728cc691ccba89 upgraded to QLineF::intersects() as QLineF::intersect() is marked deprecated since Qt 5.14.
    
    This commit adds a conditional compile time check to allow building the library with Qt versions < Qt 5.14.

--- qschematic/wire_system/wire.cpp
+++ qschematic/wire_system/wire.cpp
@@ -93,7 +93,11 @@ void wire::move_junctions_to_new_segment(const line& oldSegment, const line& new
                 // Only move in the direction of the segment if it is hor. or vert.
                 if (!junctionSeg.is_null()) {
                     QPointF intersection;
-                    auto type = junctionSeg.toLineF().intersects(newSegment.toLineF(), &intersection);
+#                   if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
+                        auto type = junctionSeg.toLineF().intersects(newSegment.toLineF(), &intersection);
+#                   else
+                        auto type = junctionSeg.toLineF().intersect(newSegment.toLineF(), &intersection);
+#                   endif
                     if (type != QLineF::NoIntersection) {
                         wire->move_point_by(jIndex, QVector2D(intersection - point.toPointF()));
                     }
