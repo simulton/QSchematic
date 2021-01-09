4ba5cc5252a76608b16fc79fb9d803a5828e1bed
Joel Bodenmann <joel@embedded.pro>
2021-01-09 16:14:15 +0100

    Improve the way that the GPDS dependency is handled

--- qschematic/external.cmake
+++ qschematic/external.cmake
@@ -3,23 +3,17 @@ include(FetchContent)
 # GPDS
 if (DEPENDENCY_GPDS_FROM_SYSTEM)
     find_package(gpds 0.1.0 REQUIRED)
+
+    set(DEPENDENCY_GPDS_TARGET "gpds::gpds-shared")
 else()
     FetchContent_Declare(
         gpds
         GIT_REPOSITORY https://github.com/simulton/gpds
         GIT_TAG        develop
     )
+    FetchContent_MakeAvailable(gpds)
 
-    FetchContent_GetProperties(gpds)
-    if(NOT gpds_POPULATED)
-        FetchContent_Populate(gpds)
-        add_subdirectory(${gpds_SOURCE_DIR} ${gpds_BINARY_DIR} EXCLUDE_FROM_ALL)
-    endif()
-
-    # Create alias to allow uniform access through the namespace
-    add_library(gpds::gpds-objs ALIAS gpds-objs)
-    add_library(gpds::gpds-static ALIAS gpds-static)
-    add_library(gpds::gpds-shared ALIAS gpds-shared)
+    set(DEPENDENCY_GPDS_TARGET "gpds-shared")
 endif()
 
 # Qt5
