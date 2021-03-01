28e7bbcdfeb8f99418cadd7d089f68f2a077671d
Joel Bodenmann <joel@embedded.pro>
2021-03-01 10:42:12 +0100

    Improve cmake Improve the way that the GPDS dependency is pulled into the system. Get rid of `DEPENDENCY_GPDS_FROM_SYSTEM` and introduce `DEPENDENCY_GPDS_DOWNLOAD` instead.

--- qschematic/external.cmake
+++ qschematic/external.cmake
@@ -1,11 +1,7 @@
 include(FetchContent)
 
 # GPDS
-if (DEPENDENCY_GPDS_FROM_SYSTEM)
-    find_package(gpds 0.1.0 REQUIRED)
-
-    set(DEPENDENCY_GPDS_TARGET "gpds::gpds-shared")
-else()
+if (DEPENDENCY_GPDS_DOWNLOAD)
     FetchContent_Declare(
         gpds
         GIT_REPOSITORY https://github.com/simulton/gpds
