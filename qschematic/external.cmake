ead01792cb8eb65104c61c0d8e99c61aaab89590
Joel Bodenmann <joel@embedded.pro>
2021-03-03 00:26:14 +0100

    Dependency GPDS: Do not builds tests and examples.

--- qschematic/external.cmake
+++ qschematic/external.cmake
@@ -7,7 +7,13 @@ if (DEPENDENCY_GPDS_DOWNLOAD)
         GIT_REPOSITORY https://github.com/simulton/gpds
         GIT_TAG        master
     )
-    FetchContent_MakeAvailable(gpds)
+    FetchContent_GetProperties(gpds)
+    if(NOT gpds_POPULATED)
+        FetchContent_Populate(gpds)
+        set(GPDS_BUILD_TESTS OFF CACHE INTERNAL "")
+        set(GPDS_BUILD_EXAMPLES OFF CACHE INTERNAL "")
+        add_subdirectory(${gpds_SOURCE_DIR} ${gpds_BINARY_DIR})
+    endif()
 endif()
 
 # Qt5
