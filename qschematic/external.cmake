e1063d7fd08f6de8e69461d6e1de2f62ee0345ee
Joel Bodenmann <joel.bodenmann@simulton.com>
2021-12-03 14:58:09 +0100

    cmake: update GPDS dependency from 1.1.0 to 1.2.0

--- qschematic/external.cmake
+++ qschematic/external.cmake
@@ -5,13 +5,14 @@ if (QSCHEMATIC_DEPENDENCY_GPDS_DOWNLOAD)
     FetchContent_Declare(
         gpds
         GIT_REPOSITORY https://github.com/simulton/gpds
-        GIT_TAG        1.1.0
+        GIT_TAG        1.2.0
     )
     FetchContent_GetProperties(gpds)
     if(NOT gpds_POPULATED)
         FetchContent_Populate(gpds)
-        set(GPDS_BUILD_TESTS OFF CACHE INTERNAL "")
+        set(GPDS_BUILD_TESTS    OFF CACHE INTERNAL "")
         set(GPDS_BUILD_EXAMPLES OFF CACHE INTERNAL "")
+        set(GPDS_FEATURE_SPDLOG OFF CACHE INTERNAL "")
         add_subdirectory(${gpds_SOURCE_DIR} ${gpds_BINARY_DIR})
     endif()
 endif()
