86087f2083fed38bc71b29b528fbb4475fd7a7b8
Joel Bodenmann <jbo@insane.engineer>
2022-06-08 15:51:07 +0000

    dependency/gpds: update to 1.5.0 when downloading

--- qschematic/external.cmake
+++ qschematic/external.cmake
@@ -5,7 +5,7 @@ if (QSCHEMATIC_DEPENDENCY_GPDS_DOWNLOAD)
     FetchContent_Declare(
         gpds
         GIT_REPOSITORY https://github.com/simulton/gpds
-        GIT_TAG        1.4.0
+        GIT_TAG        1.5.0
     )
     FetchContent_GetProperties(gpds)
     if(NOT gpds_POPULATED)
