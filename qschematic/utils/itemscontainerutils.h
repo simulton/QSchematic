e87953430c4b14df121491c71096ea20e54e1f39
Joel Bodenmann <jbo@insane.engineer>
2022-10-11 14:25:54 +0200

    codingstyle

--- qschematic/utils/itemscontainerutils.h
+++ qschematic/utils/itemscontainerutils.h
@@ -10,25 +10,25 @@
 namespace QSchematic::ItemUtils
 {
 
-template <
-    template <typename... T> typename OutContainerT = std::vector,
-    typename ContainerT
+    template <
+        template <typename... T> typename OutContainerT = std::vector,
+        typename ContainerT
     >
-auto mapItemListToSharedPtrList(ContainerT itemList) -> OutContainerT<std::shared_ptr<Item>>
-{
-    OutContainerT<std::shared_ptr<Item>> out;
+    [[nodiscard]]
+    OutContainerT<std::shared_ptr<Item>>
+    mapItemListToSharedPtrList(ContainerT itemList)
+    {
+        OutContainerT<std::shared_ptr<Item>> out;
 
-    if constexpr (!std::is_same_v<OutContainerT<Item>, QList<Item>>) {
-        out.reserve(itemList.count());
-    }
+        if constexpr (!std::is_same_v<OutContainerT<Item>, QList<Item>>)
+            out.reserve(itemList.count());
 
-    for (auto& item : itemList) {
-        if (auto qsitem = qgraphicsitem_cast<Item*>(item)) {
-            out.push_back(qsitem->sharedPtr());
+        for (auto& item : itemList) {
+            if (auto qsitem = qgraphicsitem_cast<Item*>(item))
+                out.push_back(qsitem->sharedPtr());
         }
-    }
 
-    return out;
-}
+        return out;
+    }
 
 }
--- qschematic/utils/itemscustodian.h
+++ qschematic/utils/itemscustodian.h
@@ -7,55 +7,52 @@
 #include <unordered_map>
 #include <memory>
 
-namespace QSchematic {
-
-namespace ItemUtils
+namespace QSchematic::ItemUtils
 {
 
-template <typename CustodyItemT>
-class ItemsCustodian
-{
-public:
-    /**
-     * Make sure parent is removed for all items in custody, since they will be
-     * deleted by managed pointer, to ensure no double delete occurs by Qt
-     */
-    ~ItemsCustodian()
+    template <typename CustodyItemT>
+    class ItemsCustodian
     {
-        if constexpr (std::is_base_of_v<QSchematic::Item, CustodyItemT>) {
-            for ( auto const& item : _custody_items ) {
-                item->setParentItem(nullptr);
+    public:
+        /**
+         * Make sure parent is removed for all items in custody, since they will be
+         * deleted by managed pointer, to ensure no double delete occurs by Qt
+         */
+        ~ItemsCustodian()
+        {
+            if constexpr (std::is_base_of_v<QSchematic::Item, CustodyItemT>) {
+                for ( auto const& item : _custody_items )
+                    item->setParentItem(nullptr);
+            }
+            else if constexpr (std::is_base_of_v<QObject, CustodyItemT>) {
+                for ( auto const& item : _custody_items )
+                    item->setParent(nullptr);
+            }
+            else {
+                return;
             }
         }
-        else if constexpr (std::is_base_of_v<QObject, CustodyItemT>) {
-            for ( auto const& item : _custody_items ) {
-                item->setParent(nullptr);
-            }
+
+        void
+        takeCustody(std::shared_ptr<CustodyItemT> item)
+        {
+            _custody_items.insert_or_assign(item.get(), item);
         }
-        else {
-            return;
+
+        void
+        releaseCustody(std::shared_ptr<CustodyItemT> item)
+        {
+            _custody_items.erase(item.get());
         }
-    }
 
-    auto takeCustody(std::shared_ptr<CustodyItemT> item) -> void
-    {
-        _custody_items.insert_or_assign(item.get(), item);
-    }
+        void clear()
+        {
+            _custody_items.clear();
+        }
 
-    auto releaseCustody(std::shared_ptr<CustodyItemT> item) -> void
-    {
-        _custody_items.erase(item.get());
-    }
+    private:
+        std::unordered_map<CustodyItemT, std::shared_ptr<CustodyItemT>> _custody_items;
 
-    auto clear() -> void {
-        _custody_items.clear();
-    }
-
-private:
-    std::unordered_map<CustodyItemT, std::shared_ptr<CustodyItemT>> _custody_items;
-
-};
-
-}
+    };
 
 }
