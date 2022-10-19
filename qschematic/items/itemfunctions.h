24825b766d26bcc02affbff7d56368d71be3f7cf
Joel Bodenmann <jbo@insane.engineer>
2022-10-19 15:24:32 +0200

    coding style

--- qschematic/items/itemfunctions.h
+++ qschematic/items/itemfunctions.h
@@ -3,26 +3,34 @@
 #include <QGraphicsItem>
 #include <QGraphicsScene>
 
-namespace QSchematic {
+namespace QSchematic
+{
 
-template <typename T>
-inline auto dissociate_item(std::shared_ptr<T> item) -> void {
-    dissociate_item(item.get());
-}
-
-inline auto dissociate_item(QGraphicsItem* item) -> void {
-    item->setParentItem(nullptr);
-
-    if ( auto scene = item->scene() ) {
-        scene->removeItem(item);
-    }
-}
-
-template <typename T, template <typename ValT> typename ContainerT>
-inline auto dissociate_items(ContainerT<std::shared_ptr<T>> items) -> void {
-    for ( auto item : items ) {
+    template <typename T>
+    inline
+    void
+    dissociate_item(std::shared_ptr<T> item)
+    {
         dissociate_item(item.get());
     }
-}
+
+    inline
+    void
+    dissociate_item(QGraphicsItem* item)
+    {
+        item->setParentItem(nullptr);
+
+        if (auto scene = item->scene())
+            scene->removeItem(item);
+    }
+
+    template <typename T, template <typename ValT> typename ContainerT>
+    inline
+    void
+    dissociate_items(ContainerT<std::shared_ptr<T>> items)
+    {
+        for (auto item : items)
+            dissociate_item(item.get());
+    }
 
 }
