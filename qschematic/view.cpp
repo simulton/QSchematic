simulton/QSchematic

commit 9dd8dbe69624d317f319ae79fead592df420f4a7

view: compensate for exponential zoom
This change makes zooming appear linearly to the user.

Fixes #35

qschematic/view.cpp
@@ -1,6 +1,7 @@
 #include <QKeyEvent>
 #include <QWheelEvent>
 #include <QScrollBar>
+#include <QtMath>
 
 #include "view.h"
 #include "scene.h"
@@ -109,12 +110,20 @@ void View::keyPressEvent(QKeyEvent* event)
 
 void View::wheelEvent(QWheelEvent* event)
 {
+    // CTRL + wheel to zoom
     if (event->modifiers() & Qt::ControlModifier) {
-        if (event->angleDelta().y() > 0 && _scaleFactor < ZOOM_FACTOR_MAX) {
+
+        // Zoom in (clip)
+        if (event->angleDelta().y() > 0) {
             _scaleFactor += ZOOM_FACTOR_STEPS;
-        } else if (event->angleDelta().y() < 0 && _scaleFactor > ZOOM_FACTOR_MIN) {
+        }
+
+        // Zoom out (clip)
+        else if (event->angleDelta().y() < 0) {
             _scaleFactor -= ZOOM_FACTOR_STEPS;
         }
+
+        _scaleFactor = qBound(0.0, _scaleFactor, 1.0);
 
         updateScale();
     }
@@ -196,17 +205,23 @@ void View::setSettings(const Settings& settings)
 
 void View::setZoomValue(qreal factor)
 {
-    _scaleFactor = factor;
+    _scaleFactor = qLn(ZOOM_FACTOR_MIN/factor) / qLn(ZOOM_FACTOR_MIN / ZOOM_FACTOR_MAX);
 
     updateScale();
 }
 
 void View::updateScale()
 {
-    // Apply the new scale
-    setTransform(QTransform::fromScale(_scaleFactor, _scaleFactor));
+    // Exponential interpolation
+    float logMinZoom = qLn(ZOOM_FACTOR_MIN);
+    float logMaxZoom = qLn(ZOOM_FACTOR_MAX);
+    float logZoom = logMinZoom + (logMaxZoom - logMinZoom) * _scaleFactor;
+    float zoom = qExp(logZoom);
 
-    emit zoomChanged(_scaleFactor);
+    // Apply the new scale
+    setTransform(QTransform::fromScale(zoom, zoom));
+
+    emit zoomChanged(zoom);
 }
 
 void View::setMode(Mode newMode)

commit 7332764c308a7be300c075dec86213442b4ce562

view: fix fitInView()
This was broken in commit 9dd8dbe69624d317f319ae79fead592df420f4a7

Ref #35

qschematic/view.cpp
@@ -257,12 +257,12 @@ void View::fitInView()
 
     // Update and cap the scale factor
     qreal currentScaleFactor = _scaleFactor;
+    qreal newScaleFactor = _scaleFactor;
     QGraphicsView::fitInView(rect, Qt::KeepAspectRatio);
-    _scaleFactor = viewport()->geometry().width() / mapToScene(viewport()->geometry()).boundingRect().width();
-    if (currentScaleFactor < 1) {
-        _scaleFactor = std::min(_scaleFactor, 1.0);
-    } else {
-        _scaleFactor = std::min(_scaleFactor, currentScaleFactor);
-    }
-    updateScale();
+    newScaleFactor = viewport()->geometry().width() / mapToScene(viewport()->geometry()).boundingRect().width();
+    if (currentScaleFactor < 1)
+        newScaleFactor = std::min(newScaleFactor, 1.0);
+    else
+        newScaleFactor = std::min(newScaleFactor, currentScaleFactor);
+    setZoomValue(newScaleFactor);
 }