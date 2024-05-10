a50d5b31e7dc1c6079a071881da98aca1b438bf0
Joel Bodenmann <jbo@insane.engineer>
2024-05-10 18:56:53 +0200

    scene: Only render exposed portion of background
    Fixes #70

--- qschematic/background.cpp
+++ qschematic/background.cpp
@@ -22,8 +22,6 @@ Background::setSettings(const Settings& settings)
 void
 Background::paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget)
 {
-    // ToDo: Use QStyleOptionGraphicsItem::exposedRect
-
     // Background pen
     QPen backgroundPen;
     backgroundPen.setStyle(Qt::NoPen);
@@ -44,30 +42,27 @@ Background::paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWi
     QBrush gridBrush;
     gridBrush.setStyle(Qt::NoBrush);
 
-    // Get the rects
-    //
-    // sr = scene rect
-    // ep = exposed rect
-    const QRectF sr = rect();
+    // Get the rectangle of interest (er = "exposed rect")
     const QRectF er = (option ? option->exposedRect : rect());
 
+    // Prepare painter
     painter->save();
     painter->setRenderHint(QPainter::Antialiasing, m_settings.antialiasing);
 
     // Draw background
     painter->setPen(backgroundPen);
     painter->setBrush(backgroundBrush);
-    painter->drawRect(rect());
+    painter->drawRect(er);
 
     // Draw the grid if supposed to
     if (m_settings.showGrid && (m_settings.gridSize > 0)) {
-        qreal left = int(rect().left()) - (int(rect().left()) % m_settings.gridSize);
-        qreal top = int(rect().top()) - (int(rect().top()) % m_settings.gridSize);
+        const qreal left = int(er.left()) - (int(er.left()) % m_settings.gridSize);
+        const qreal top = int(er.top()) - (int(er.top()) % m_settings.gridSize);
 
         painter->setPen(gridPen);
         painter->setBrush(gridBrush);
-        for (qreal x = left; x < sr.right(); x += m_settings.gridSize) {
-            for (qreal y = top; y < sr.bottom(); y += m_settings.gridSize)
+        for (qreal x = left; x < er.right(); x += m_settings.gridSize) {
+            for (qreal y = top; y < er.bottom(); y += m_settings.gridSize)
                 painter->drawPoint(x, y);
         }
     }
