e976ba7554d24fe81fb14ff50306e0af6bc76d80
Joel Bodenmann <jbo@insane.engineer>
2025-02-19 14:14:13 +0100

    cmake: Update to comply with CMP0169

--- qschematic/external.cmake
+++ qschematic/external.cmake
@@ -4,19 +4,16 @@ include(FetchContent)
 # GPDS
 ########################################################################################################################
 if (QSCHEMATIC_DEPENDENCY_GPDS_DOWNLOAD)
+    set(GPDS_BUILD_TESTS    OFF CACHE INTERNAL "")
+    set(GPDS_BUILD_EXAMPLES OFF CACHE INTERNAL "")
+    set(GPDS_FEATURE_SPDLOG OFF CACHE INTERNAL "")
+
     FetchContent_Declare(
         gpds
         GIT_REPOSITORY https://github.com/simulton/gpds
         GIT_TAG        ${QSCHEMATIC_DEPENDENCY_GPDS_DOWNLOAD_VERSION}
     )
-    FetchContent_GetProperties(gpds)
-    if(NOT gpds_POPULATED)
-        FetchContent_Populate(gpds)
-        set(GPDS_BUILD_TESTS    OFF CACHE INTERNAL "")
-        set(GPDS_BUILD_EXAMPLES OFF CACHE INTERNAL "")
-        set(GPDS_FEATURE_SPDLOG OFF CACHE INTERNAL "")
-        add_subdirectory(${gpds_SOURCE_DIR} ${gpds_BINARY_DIR})
-    endif()
+    FetchContent_MakeAvailable(gpds)
 else()
     find_package(
         gpds
